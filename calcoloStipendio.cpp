40025becd731c1fec04fbe13eb6830e6ed3d2579
Jacopo <jcp.federico@gmail.com>
2023-02-24 16:24:35 +0100

    modifiche varie esercizio calcoloStipendio finito

--- calcoloStipendio.cpp
+++ calcoloStipendio.cpp
@@ -2,118 +2,113 @@
 #include <fstream>
 using namespace std;
 
+string cerca(string nomeFile, string code)
+{
+    string risultato = "";
+    ifstream fileAperto(nomeFile);
+    if(fileAperto.is_open())
+    {
+        string line;
+        while(getline(fileAperto, line))
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                if(line[i] != code[i])
+                {
+                    break;
+                }
+                else if(i == 4) // Ultimo carattere della matricola
+                {
+                    for(int j = 6; j < line.length(); j++)
+                    {
+                        risultato += line[j];
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        cout << "Impossibile aprire il file .txt . \n";
+    }
+    return risultato;
+}
+
 void findNameByCode(string code)
 {
-    string nome = "";
-    ifstream fileMatricole("matricola.txt"); // Apro il file giusto
-    if(fileMatricole.is_open())
-    {
-        cout << "File matricole.txt aperto correttamente. \n";
-        string line;
-        while(getline(fileMatricole, line)) // Scorro le righe
-        {
-            for(int i = 0; i < 6; i++) // Per ogni carattere della riga 0....5
-            {
-                if(line[i] != code[i])
-                {
-                    break; // Se non uguali passo a riga successiva
-                }
-                else if(i == 4) // Ho trovato la matricola
-                {
-                    for(int j = 6; j < line.length(); j++)
-                    {
-                        nome += line[j];
-                    }               
-                }
-            }
-        }
-        cout << "La matricola " << code << " corrisponde al nome: " << nome << "\n";
-    }
-    else
-    {
-        cout << "Impossibile aprire il file matricole.txt . \n";
-    }
+   string risultato = cerca("matricola.txt", code);
+   cout << "La matricola " << code << " corrisponde al nome dell'impiegato " << risultato << "\n";
 }
 
-void findSalaryByCode(string code) 
+void findSalaryByCode(string code)
 {
-    string stipendio = "";
-    ifstream fileStipendio("stipendio.txt"); // Apro file stipendio.txt
-    if(fileStipendio.is_open())
-    {
-        cout << "File stipendio.txt aperto correttamente. \n";
-        string line;
-        while(getline(fileStipendio, line)) // Scorro le righe
-        {
-            for(int i = 0; i < 6; i++) // Per ogni carattere della riga 0....5
-            {
-                if(line[i] != code[i])
-                {
-                    break; // Se non uguali passo a riga successiva
-                }
-                else if(i == 4) // Ho trovato lo stipendio
-                {
-                    for(int j = 6; j < line.length(); j++)
-                    {
-                        stipendio += line[j];
-                    }               
-                }
-            }
-        }
-        cout << "Alla matricola " << code << " corrisponde lo stipendio: " << stipendio << "\n";
-    }
-    else
-    {
-        cout << "Impossibile aprire il file stipendio.txt . \n";
-    }
+    cout << "Alla matricola " << code << " corrisponde uno stipendio di " << cerca("stipendio.txt", code) << ". \n";
+}   
+
+void findStraordinaryByCode(string code) // Apro file stipendio.txt
+{
+    cout << "La matricola " << code << " ha maturato " << cerca("straordinari.txt", code) << " ore di straordinario. \n";
 }
 
-void findSalaryByName(string name) // Apro file stipendio.txt
+void findSalaryByName(string name)
 {
-    string straordinario = "";
-    ifstream fileStraordinari("straordinari.txt"); // Apro il file giusto
-    if(fileStraordinari.is_open())
+    ifstream fileAperto("matricola.txt");
+    string code = "";
+    if(fileAperto.is_open())
     {
-        cout << "File straordinari.txt aperto correttamente. \n";
         string line;
-        while(getline(fileStraordinari, line)) // Scorro le righe
+        while(getline(fileAperto, line))
         {
-            for(int i = 0; i < 6; i++) // Per ogni carattere della riga 0....5
+            int k = 0;
+            for(int i = 6; i < line.length(); i++)
             {
-                if(line[i] != name[i])
+                if(line[i] != name[k])
                 {
-                    break; // Se non uguali passo a riga successiva
+                    break;
                 }
-                else if(i == 4) // Ho trovato la matricola
+                else if(k == name.length() -1) // Ultimo carattere della matricola
                 {
-                    for(int j = 6; j < line.length(); j++)
+                    for (int j = 0; j < 5; j++)
                     {
-                        straordinario += line[j];
-                    }               
+                        code += line[j];
+                    }
                 }
+                k++;
             }
         }
-        cout << "L'impiegato " << name << " ha accumulato " << straordinario << "ore di straordinario\n";
+        fileAperto.close();
     }
     else
     {
-        cout << "Impossibile aprire il file matricole.txt . \n";
+        cout << "Impossibile aprire il file .txt .\n";
     }
+    string stipendioValore = cerca("stipendio.txt", code);
+    cout << "Il dipendente " << name << ", avente matricola " << code << ", percepisce uno stipendio di " << stipendioValore << ". \n";
+}
+
+void findSalaryAndStraordinaryByCode(string code)
+{
+    int retribuzione = stoi(cerca("stipendio.txt", code));
+    int compensoExtra = stoi(cerca("straordinari.txt", code));
+    int retribuzioneTot = retribuzione + compensoExtra * 35;
+    cout << "Lo stipendio, comprensivo di straordinari, del dipendente avente matricola " << code << " ammonta a " << retribuzioneTot << ". \n";
 }
 
 int main()
 {
     cout << "Benvenuto nel programma di calcolo stipendi. \n";
 
-    bool flag = true; 
+    bool flag = true;
     while (flag) // Ciclo inserimento Menù
     {
         cout << "----------------------------------------------------------\n";
-        cout << "Queste sono le funzionalità che puoi usare: \n";
-        cout << "1. Matricola  ->   Nome \n";
-        cout << "2. Matricola  -> Stipendio \n";
-        cout << "3.   Nome     -> Stipendio \n";
-        cout << "4. "
+        cout << "Queste sono le funzionalità che puoi usare: \n";     
+        cout << "1. Matricola  ->             Nome \n";
+        cout << "2. Matricola  ->          Stipendio \n";
+        cout << "3. Matricola  ->         Straordinari \n";
+        cout << "4.   Nome     ->          Stipendio \n";
+        cout << "5. Matricola  ->   Stipendio + Straordinari (35h) \n";
+        cout << "0.   EXIT \n";
         cout << "----------------------------------------------------------\n";
         cout << "Scegli la funzionalità che vuoi usare: \n";
         int scelta;
@@ -122,7 +117,7 @@ int main()
         string input;
         cin >> input;
 
-        if (scelta < 1 || scelta > 3)
+        if (scelta < 0 || scelta > 5)
         {
             cout << "Scelta non valida. \n";
         }
@@ -131,17 +126,26 @@ int main()
             if (scelta == 1 && input.length() == 5)
             {
                 findNameByCode(input);
-                flag = false; // Mi fa finire il programma
             }
             else if (scelta == 2 && input.length() == 5)
             {
                 findSalaryByCode(input);
-                flag = false; // Mi fa finire il programma
             }
             else if (scelta == 3)
             {
+                findStraordinaryByCode(input);
+            }
+            else if(scelta == 4)
+            {
                 findSalaryByName(input);
-                flag = false; // Mi fa finire il programma
+            }
+            else if(scelta == 5)
+            {
+                findSalaryAndStraordinaryByCode(input);
+            }
+            else if(scelta == 0)
+            {
+                flag = false;
             }
             else
             {
